7d379e8b523061b6c6f428a31eb815fe4ccd934a
emalsoria <emalsoria@gmail.com>
2016-06-28 18:29:11 +0200

    Correction plugin

--- src/windows/usernamepluginProxy.js
+++ src/windows/usernamepluginProxy.js
@@ -2,8 +2,26 @@
 cordova.commandProxy.add("UserName", {
 
     userName: function (successCallback, errorCallback) {
-    	Windows.System.UserProfile.UserInformation.getDisplayNameAsync().then(function(displayName) {
-			successCallback(displayName);
-		})
+        var System = Windows.System;
+        var User = System.User;
+        var KnownUserProperties = System.KnownUserProperties;
+
+        User.findAllAsync().then(function (users) {
+            WinJS.Promise.join(users.map(function (user) {
+                return user.getPropertiesAsync([KnownUserProperties.domainName,
+                    KnownUserProperties.lastName,
+                    KnownUserProperties.firstName,
+                    KnownUserProperties.providerName,
+                    KnownUserProperties.accountName,
+                    KnownUserProperties.guestHost,
+                    KnownUserProperties.principalName,
+                    KnownUserProperties.sessionInitiationProtocolUri]);
+            })).then(function (results) {
+                for (var i = 0; i < users.length; i++) {
+                    successCallback(results[i]);
+                    break;
+                }
+            });
+        });
     }
 });
